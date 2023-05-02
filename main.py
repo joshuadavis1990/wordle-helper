2e4fe6de02532ffca20e8666da6a97753c76915b
joshuadavis1990 <joshuadavis1990@me.com>
2023-05-02 20:55:33 +1000

    Add pseudocode control flow plan for first iteration

---
+++ main.py
@@ -0,0 +1,9 @@
+# The purpose of this application is to assist the user in solving the daily Wordle challenge at https://www.nytimes.com/games/wordle/index.html. In order to meet the assessment brief, it will need to include: user input, printed output, variables and variable scope, loops and conditional control structures, six or more simple functions, error handling, four or more Python packages, functions from one or more Python packages, and two tests.
+
+# First iteration of app - focus on correct letters only:
+# - Import a dictionary or list of words - narrow words to 5 letter words only
+# - User enters their first guess
+# - User enters their results in a defined format, e.g. Y = correct, N = incorrect e.g. YYNNY?
+# Iterate through the word list/imported dictionary to find words that match the pattern the user has input
+# - Repeat the above process six times or until the correct word is found
+# - Remind user to type quit to exit the app at any point

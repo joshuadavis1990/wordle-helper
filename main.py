ab28319030ce342d3db0af538121231131869429
joshuadavis1990 <joshuadavis1990@me.com>
2023-05-08 11:41:19 +1000

    Add algorithms to rank commonality of letters/words in word-bank.txt

--- main.py
+++ main.py
@@ -1,3 +1,64 @@
+from collections import Counter
+from itertools import chain
+import operator
+
+word_length = 5
+guesses = 6
+
+# Import all possible Wordle words from work-bank.txt
+word_candidates = []
+with open("word-bank.txt") as words:
+    for line in words:
+        word_candidates.append(line.strip())
+
+# Algorithm to find most common letters in word-bank.txt
+letter_counter = Counter(chain.from_iterable(word_candidates))
+
+# Algorithm to split most common letters into percentages of the overall total
+letter_frequency = {
+    character: value / letter_counter.total()
+    for character, value in letter_counter.items()
+}
+
+# Word scoring function for commonality of letters in word
+def word_commonality(word):
+    score = 0.0
+    for character in word:
+        score += letter_frequency[character]
+    return score / (word_length - len(set(word)) + 1)
+
+# Sorts and displays words to user
+def sort_by_word_commonality(words):
+    sort_by = operator.itemgetter(1)
+    return sorted(
+        [(word, word_commonality(word)) for word in words],
+        key=sort_by,
+        reverse=True,
+    )
+
+def display_word_table(word_commonalities):
+    for (word, freq) in word_commonalities:
+        print(f"{word:<10} | {freq:<5.2}")
+
+# print(display_word_table(sort_by_word_commonality(word_candidates)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PSEUDOCODE
+
 # The purpose of this application is to assist the user in solving the daily Wordle challenge at https://www.nytimes.com/games/wordle/index.html. In order to meet the assessment brief, it will need to include: user input, printed output, variables and variable scope, loops and conditional control structures, six or more simple functions, error handling, four or more Python packages, functions from one or more Python packages, and two tests.
 
 # First iteration of app - focus on correct letters only:
@@ -8,16 +69,7 @@
 # - Repeat the above process six times or until the correct word is found
 # - Remind user to type quit to exit the app at any point
 
-word_candidates = []
-with open("word-bank.txt") as words:
-    for line in words:
-        word_candidates.append(line.strip())
-# print(word_candidates[0:10])
-
-# choose_a_word = "court"
-# for word in word_candidates:
-#     if word == choose_a_word:
-#         print(word)
+# TEST FUNCTIONS
 
 def choose_letter(letter):
     for word in word_candidates:
@@ -41,14 +93,16 @@ def letter_positions(let1, let1_position, let2, let2_position):
     for word in word_candidates:
         if word[let1_position - 1] == let1.lower() and word[let2_position - 1] == let2.lower():
             print(word)
-            
+
 # let1 = input("Letter 1: ")
 # let1_position = int(input("Position: "))
 # let2 = input("Letter 2: ")
 # let2_position = int(input("Position: "))
 # letter_positions(let1, let1_position, let2, let2_position)
 
-word_entered = input("Word entered: ")
-result = input("Result in XXXGG format: ")
-together = zip(result, word_entered)
-print(list(together))
+# word_entered = input("Word entered: ")
+# result = input("Result in XXXGG format: ")
+# together = zip(result, word_entered)
+
+# for result, word_entered in together:
+#     print(list(together))

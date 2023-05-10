a64e67acfba9483f926d6ba84ed1463f930f8af4
joshuadavis1990 <joshuadavis1990@me.com>
2023-05-10 11:54:37 +1000

    Add functions to calculate the frequency of letters in word-bank.txt and the word_score of each word based on its letters

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ colorama.init(autoreset=True)
 from colorama import Fore, Back, Style
 from collections import Counter
 from itertools import chain
+import operator
 
 # GLOBAL VARIABLES
 
@@ -23,6 +24,13 @@ try:
 except FileNotFoundError:
     print("File not found.")
 
+letter_counter = Counter(chain.from_iterable(word_candidates))
+
+letter_frequency = {
+    character: value / letter_counter.total()
+    for character, value in letter_counter.items()
+}
+
 # APP FUNCTIONS
 
 def app_start():
@@ -33,6 +41,24 @@ def app_start():
     print(f"When you enter each result, remember the code: {Style.BRIGHT}{Fore.GREEN}G for Green {Fore.YELLOW}Y for Yellow {Fore.WHITE}X for Gray.")
     print(f"\nHere are some random words from Wordle's word bank you might like to choose from: \n{random.choices(word_candidates, k=8)}")
     print("\n----------------------------------------------------------------")
+
+def calculate_word_commonality(word):
+    score = 0.0
+    for char in word:
+        score += letter_frequency[char]
+    return score / (word_length - len(set(word)) + 1)
+
+def sort_words(words):
+    sort = operator.itemgetter(1)
+    return sorted(
+        [(word, calculate_word_commonality(word)) for word in words],
+        key = sort,
+        reverse = True,
+    )
+
+def display(word_commonalities):
+    for word, distribution in word_commonalities:
+        print(f"{word:<10} | {distribution:<5.2}")
 
 def word_entered():
     word = input("Word entered: ")
@@ -48,7 +74,9 @@ def user_response():
 
 app_start()
 for attempt in range(1, allowed_attempts + 1):
+    temp_list = tuple(word_candidates)
     print(f"\n\n{Style.BRIGHT}{Fore.MAGENTA}Attempt {attempt} with {len(word_candidates)} possible words")
+    display(sort_words(word_candidates)[:15])
     guess = word_entered()
     feedback = user_response()
     if feedback == "GGGGG" and attempt == 1:
@@ -57,7 +85,6 @@ for attempt in range(1, allowed_attempts + 1):
     elif feedback == "GGGGG":
         print(f"\nWell done! You took {attempt} attempts!")
         break
-    temp_list = tuple(word_candidates)
     for word in temp_list:
         for i in range(word_length):
             if feedback[i] == "X" and guess[i] in word:

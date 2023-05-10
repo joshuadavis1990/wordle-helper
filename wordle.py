102e58867d0872ed05a723a6cce71c110b307f4d
joshuadavis1990 <joshuadavis1990@me.com>
2023-05-10 20:15:48 +1000

    Add KeyboardInterrupt shortcut to application

--- wordle.py
+++ wordle.py
@@ -17,6 +17,7 @@ word_candidates = []
 allowed_attempts = 6
 word_length = 5
 allowed_code = ("G", "Y", "X")
+exit_prompt = "Type 'q' at any point if you want to quit the app."
 
 try:
     with open("word-bank.txt") as words:
@@ -40,6 +41,7 @@ def app_start():
     print(f"\n{Style.BRIGHT}Welcome to Wordle Helper.\n")
     print(f"Head to {Fore.MAGENTA}https://www.nytimes.com/games/wordle/index.html{Style.RESET_ALL} and let's get started with the daily Wordle for {formatted_date}!")
     print(f"When you enter each result, remember the code: {Style.BRIGHT}{Fore.GREEN}G for Green {Fore.YELLOW}Y for Yellow {Fore.WHITE}X for Gray.")
+    print(f"{exit_prompt}")
     print(f"\nHere are some random words from Wordle's word bank you might like to choose from: \n{random.choices(word_candidates, k=8)}")
     print("\n----------------------------------------------------------------")
 
@@ -64,11 +66,13 @@ def display(word_commonalities):
 def word_entered():
     while True:
         word = input("\nWord entered: ")
+        if word.lower() == "q":
+            raise KeyboardInterrupt
         if not word.isalpha():
-            print("Please enter letters only.")
+            print(f"Please enter letters only. {exit_prompt}")
             continue
         elif len(word) != word_length:
-            print("Please enter a 5 letter word only.")
+            print(f"Please enter a 5 letter word only. {exit_prompt}")
             continue
         else:
             return word.lower()
@@ -81,14 +85,16 @@ def contains_illegal_letter(word):
 def user_response():
     while True:
         response = input(f"\nWordle response in {Style.BRIGHT}{Fore.GREEN}G{Fore.YELLOW}Y{Fore.WHITE}X {Style.RESET_ALL}formatting: ")
+        if response.lower() == "q":
+            raise KeyboardInterrupt
         if not response.isalpha():
-            print("Please enter letters only.")
+            print(f"Please enter letters only. {exit_prompt}")
             continue
         elif len(response) != word_length:
-            print("Please enter a 5 letter code.")
+            print(f"Please enter a 5 letter code. {exit_prompt}")
             continue
         elif contains_illegal_letter(response):
-            print(f"There's a mistake with your input. Remember: {Style.BRIGHT}{Fore.GREEN}G for Green {Fore.YELLOW}Y for Yellow {Fore.WHITE}X for Gray.")
+            print(f"There's a mistake with your input. Remember: {Style.BRIGHT}{Fore.GREEN}G for Green {Fore.YELLOW}Y for Yellow {Fore.WHITE}X for Gray.{Style.RESET_ALL} {exit_prompt}")
             continue
         else:
             return response.upper()
@@ -96,38 +102,41 @@ def user_response():
 # MAIN CODE
 
 app_start()
-for attempt in range(1, allowed_attempts + 1):
-    temp_list = tuple(word_candidates)
-    print(f"\n\n{Style.BRIGHT}{Fore.MAGENTA}Attempt {attempt} with {len(word_candidates)} possible words")
-    print(f"\nHere are the best choices out of {len(word_candidates)} possible words calculated based on the frequency of letters in the Wordle word bank.")
-    display(sort_words(word_candidates)[:6])
-    guess = word_entered()
-    feedback = user_response()
-    if feedback == "GGGGG" and attempt == 1:
-        print(f"\nAmazing effort! Guessing the Wordle in {attempt} attempt... What's your secret?")
-        break
-    elif feedback == "GGGGG":
-        print(f"\nWell done! Solving today's Wordle took {attempt} attempts.")
-        break
-    for word in temp_list:
-        for i in range(word_length):
-            if feedback[i] == "X" and guess[i] in word:
-                word_candidates.remove(word)
-                break
-            elif feedback[i] == "G" and guess[i] != word[i]:
-                word_candidates.remove(word)
-                break
-            elif feedback[i] == "Y" and guess[i] not in word:
-                word_candidates.remove(word)
-                break
-            elif feedback[i] == "Y" and guess[i] == word[i]:
-                word_candidates.remove(word)
-                break
-    counter = 0
-    print("\nWord possibilities:")
-    for word in word_candidates:
-        print(word, end = " | ")
-        counter += 1
-        if counter == 10:
-            print("")
-            counter = 0
+try:
+    for attempt in range(1, allowed_attempts + 1):
+        temp_list = tuple(word_candidates)
+        print(f"\n\n{Style.BRIGHT}{Fore.MAGENTA}Attempt {attempt} with {len(word_candidates)} possible words")
+        print(f"\nHere are the best choices out of {len(word_candidates)} possible words calculated based on the frequency of letters in the Wordle word bank.")
+        display(sort_words(word_candidates)[:6])
+        guess = word_entered()
+        feedback = user_response()
+        if feedback == "GGGGG" and attempt == 1:
+            print(f"\nAmazing effort! Guessing the Wordle in {attempt} attempt... What's your secret?")
+            break
+        elif feedback == "GGGGG":
+            print(f"\nWell done! Solving today's Wordle took {attempt} attempts.")
+            break
+        for word in temp_list:
+            for i in range(word_length):
+                if feedback[i] == "X" and guess[i] in word:
+                    word_candidates.remove(word)
+                    break
+                elif feedback[i] == "G" and guess[i] != word[i]:
+                    word_candidates.remove(word)
+                    break
+                elif feedback[i] == "Y" and guess[i] not in word:
+                    word_candidates.remove(word)
+                    break
+                elif feedback[i] == "Y" and guess[i] == word[i]:
+                    word_candidates.remove(word)
+                    break
+        counter = 0
+        print("\nWord possibilities:")
+        for word in word_candidates:
+            print(word, end = " | ")
+            counter += 1
+            if counter == 10:
+                print("")
+                counter = 0
+except KeyboardInterrupt:
+    print("\nThanks for using Wordle Helper. See you next time.")

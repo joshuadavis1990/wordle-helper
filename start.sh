332c0ca69e8cfd7d8a9b719d73ddd32aa88c4964
joshuadavis1990 <joshuadavis1990@me.com>
2023-05-12 15:17:26 +1000

    Add echo commands to bash script

--- start.sh
+++ start.sh
@@ -5,16 +5,19 @@ then
     pyv="$(python -V 2>&1)"
     if [[ $pyv == "Python 3"* ]]
     then
-        echo "You have the correct version of Python installed."
+        echo "Check complete. You have the correct version of Python installed."
     else
         echo "Sorry, this version of Python is not supported by Wordle Helper. Please download Python 3 at: https://www.python.org/downloads/" >&2
     fi
 else
     echo "You need to install Python 3 to run Wordle Helper. You can download it here: https://www.python.org/downloads/" >&2
 fi
+echo "Creating virtual environment..."
 python3 -m venv .venv
+echo "Activating virtual environment..."
 source .venv/bin/activate
+echo "Installing dependencies..."
 pip3 install -r requirements.txt
-echo "Opening the application ..."
+echo "Opening the application..."
 python3 wordle.py
 deactivate

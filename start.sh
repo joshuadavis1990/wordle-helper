c086426e2a4d7bfc28a083d96e74bcc1c42e1563
joshuadavis1990 <joshuadavis1990@me.com>
2023-05-12 14:17:26 +1000

    Finish start.sh Bash script

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+echo "Thank you for installing Wordle Helper."
 if [[ -x "$(command -v python)" ]]
 then
     pyv="$(python -V 2>&1)"
     if [[ $pyv == "Python 3"* ]]
     then
-        python3 wordle.py
+        echo "You have the correct version of Python installed."
     else
-        echo "Sorry, this version of Python is not supported by Wordle Helper. Please download Python3 at: https://www.python.org/downloads/"
+        echo "Sorry, this version of Python is not supported by Wordle Helper. Please download Python 3 at: https://www.python.org/downloads/" >&2
     fi
 else
-    echo "You need to install Python3 to run Wordle Helper at its full potential. You can download it here: https://www.python.org/downloads/" >&2
+    echo "You need to install Python 3 to run Wordle Helper. You can download it here: https://www.python.org/downloads/" >&2
 fi
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+echo "Opening the application ..."
+python3 wordle.py
+deactivate
